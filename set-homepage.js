fc64fabae66f5004c65b546c1ab4c8751c8adb5e
Tomáš Hübelbauer <tomas@hubelbauer.net>
2023-01-03 20:22:46 +0100

    Replace set-homepage.js with a jq call in the NPM script
    jq doesn't come out of the box with macOS but it is on the GitHub Actions agents and while it cannot do an in-file replacement unlike e.g.: sed, it can be combined with cat and tee to achieve a similar behavior.

--- set-homepage.js
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-
-// Patch `homepage` in `cra/package.json` for GitHub Pages to work
-// TODO: Replace this with `jq` once I install it on my system 
-const packageJson = JSON.parse(await fs.promises.readFile('cra/package.json'));
-packageJson.homepage = 'https://tomashubelbauer.github.io/ast-localizer';
-await fs.promises.writeFile('cra/package.json', JSON.stringify(packageJson, null, 2));
